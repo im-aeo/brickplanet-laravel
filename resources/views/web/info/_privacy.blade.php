b2137dd2a3f928120e8fa3aa1b3b3b887423c3c8
FoxxoSnoot <bizzywin@gmail.com>
2022-02-06 19:57:14 +0100

    Publish Terms of Service and Privacy Policy

--- resources/views/web/info/_privacy.blade.php
+++ resources/views/web/info/_privacy.blade.php
@@ -22,4 +22,16 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 -->
 
-To be written.
+<p>{{ config('site.name') }} respects the privacy of our users. This privacy policy was created to explain how we collect, use, and disclose information that pertains to your privacy. When you access or use {{ config('site.name') }}, you signify your agreement to our privacy policy.</p>
+
+<h3>Stored Information</h3>
+<p>What does {{ config('site.name') }} store, and why?</p>
+<div><b>1. Username</b></div>
+<p>Your username is used for identification purposes only.</p>
+<div><b>2. Password</b></div>
+<p>Your password is stored securely, and cannot be retrived by anyone including us. Therefore, if you forget your password, you can only login by setting a new one via forgot password.</p>
+<div><b>3. Internet Protocol Address (IP)</b></div>
+<p>Your Internet Protocol Address is collected & encrypted upon account signup, and cannot be seen by staff. This is only collected to prevent fraudulent accounts. If you wish to have this removed from your account, please contact us.</p>
+
+<h3>Sharing Information</h3>
+<p>{{ config('site.name') }} does not share/sell user information to anyone, and never will. We do however, cooperate with requests made by members of law enforcement when provided a court order.</p>
